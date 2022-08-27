9bbff0f6fd8abd45fb968ffd01aef9d3645746c7
Andrew <andrew.2008@live.co.uk>
2022-08-27 22:44:51 +0100

    Update BTECRandomPWGenerator.ps1
    Compacted/Made more efficient

--- BTECRandomPWGenerator.ps1
+++ BTECRandomPWGenerator.ps1
@@ -1,14 +1,8 @@
 $3RandomWords = (Invoke-WebRequest "https://random-word-api.herokuapp.com/word?number=1&length=6").Content
-
-$Out1 = $3RandomWords.Replace('"','')
-$Out2 = $Out1.Replace('[','')
-$Out3 = $Out2.Replace(']','')
-$Out4 = $Out3.Replace(',','.')
-$FinalOutRandomWord = (Get-Culture).TextInfo.ToTitleCase($Out4)
+$Out1 = $3RandomWords.Replace('"','').Replace('[','').Replace(']','').Replace(',','.')
+$FinalOutRandomWord = (Get-Culture).TextInfo.ToTitleCase($Out1)
 
 $SpecialChars = Get-Random ("!","@","#","$","%","&","*","?",".")
-
-
 $Colour = Get-Random ("black","blue","gray","green","navy","orange","purple","red","silver","white","yellow")
 $Number = Get-Random -Minimum -0 -Maximum 100
 
