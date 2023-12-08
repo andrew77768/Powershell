d5f043636412dfec360e02428c1b91b351462042
Andrew <andrew.2008@live.co.uk>
2023-12-08 19:45:49 +0000

    Added Windows Server 2012 and below support.
    Support for Windows Server 2012 and below support.
    
    OpenSSL uses AES256 as a default to encrypt the private key when exporting a .pfx file.
    
    I believe AES256 is not supported on older versions of Windows.

--- OpenSSLCertRenewer.ps1
+++ OpenSSLCertRenewer.ps1
@@ -4,7 +4,6 @@
 Write-Host @"
 
 OpenSSL Certificate Renewer!
-Ensure you have the PATH environment variables set for your OpenSSL installation!
 Ensure you have the current .pfx file ready in the current directory!
 Ensure you have the new .cer file ready in the current directory!
 
@@ -21,16 +20,31 @@ Write-Host "Directory: $CWD"
 $PrivateKey = Join-Path $CWD "PrivateKey.key"
 
 $NewCertificate = Join-Path -Path $CWD *.crt
-$NewCompleteCert = Join-Path $CWD "NewCert.pfx"
+$NewCompleteCert2016 = Join-Path $CWD "NewCert2016Above.pfx"
+$NewCompleteCert2012 = Join-Path $CWD "NewCert2012Below.pfx"
 
 Write-Host "Starting Command1"
 
 invoke-expression "openssl pkcs12 -in '$OldCertificate' -nocerts -out '$PrivateKey' -passin pass:$Password -passout pass:$Password"
 
 Write-Host "Command1 finished, got private key:$PrivateKey"
+Write-Host ""
+Write-Host ""
 
 Write-Host "Starting Command2"
 
-invoke-expression "openssl pkcs12 -export -out '$NewCompleteCert' -inkey '$PrivateKey' -in '$NewCertificate' -passin pass:$Password -passout pass:$Password"
+invoke-expression "openssl pkcs12 -export -out '$NewCompleteCert2016' -inkey '$PrivateKey' -in '$NewCertificate' -passin pass:$Password -passout pass:$Password"
 
-Write-Host "Command2 finished, certificate $NewCompleteCert has been created with the default set password!"
+Write-Host "Command2 finished, certificate $NewCompleteCert2016 has been created with the default set password!"
+Write-Host ""
+Write-Host ""
+
+Write-Host "Starting Command3"
+
+invoke-expression "openssl pkcs12 -keypbe PBE-SHA1-3DES -certpbe PBE-SHA1-3DES -nomac -export -out '$NewCompleteCert2012' -inkey '$PrivateKey' -in '$NewCertificate' -passin pass:$Password -passout pass:$Password"
+
+Write-Host "Command3 finished, certificate $NewCompleteCert2012 has been created with the default set password!"
+Write-Host ""
+Write-Host ""
+
+pause
